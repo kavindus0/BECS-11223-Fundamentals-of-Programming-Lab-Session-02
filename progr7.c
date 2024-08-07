e0868d8888a091df5c6627d6c247d992b27feea2
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-08-07 14:24:58 +0530

    Question_07

---
+++ progr7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+printf("The \"UOK\" is stands for\n");
+printf("\'University of Kelaniya\'.\n");
+printf("-------\\\\ FOS \\\\ -----------\n");
+printf("Faculty of Science!!!\n");
+
+
+
+
+}
